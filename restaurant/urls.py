d7d508db7db14211af1a456dc12fcf7a40eca793
akbaruddin <akbaruddin@live.in>
2026-04-25 18:54:28 +0530

    feat: more view fro booking

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import sayHello, index
+from .views import sayHello, index, MenuItemView, SingleMenuItemView, BookingViewSet
 
 urlpatterns = [
+    path('menu/', MenuItemView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('booking/table/', BookingViewSet.as_view({'get': 'list'})),
     path('sayHello', sayHello, name='sayHello'),
     path('', index, name='index'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializer import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def sayHello(request):
@@ -7,3 +11,15 @@ def sayHello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
